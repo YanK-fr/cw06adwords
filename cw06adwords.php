6f432f30218f51e8a831524b3bb326c48fdf66f0
root <root@vps77183.ovh.net>
2014-12-11 00:32:34 +0100

    Add Facebook tracking

--- cw06adwords.php
+++ cw06adwords.php
@@ -8,11 +8,11 @@ class cw06adwords extends Module {
     public function __construct($name = NULL) {
         $this->name = 'cw06adwords';
         $this->tab = 'analytics_stats';
-        $this->version = '1.0';
+        $this->version = '1.1';
         $this->author = 'creaweb06.fr';
         parent::__construct($name);
-        $this->displayName = $this->l('Conversions Google Adwords');
-        $this->description = $this->l('Ajoute le tag de conversion pour google adwords sur la page de confirmation de commande.');
+        $this->displayName = $this->l('Conversions Google Adwords et Facebook');
+        $this->description = $this->l('Ajoute le tag de conversion pour Google Adwords et Facebook sur la page de confirmation de commande.');
     }
     
     public function install(){
@@ -22,45 +22,58 @@ class cw06adwords extends Module {
         return true;
     }
     
-    
     public function getContent(){
         
         $this->_html = '<h2>'.$this->displayName.'</h2>';
         
-        if(Tools::isSubmit('submitConfig')){
-            Configuration::updateValue('CW06ADWORDS_CONVERSION_ID',Tools::getValue('CONVERSION_ID'));
-            Configuration::updateValue('CW06ADWORDS_CONVERSION_LABEL',Tools::getValue('CONVERSION_LABEL'));
+        if(Tools::isSubmit('submitConfigGA')){
+            Configuration::updateValue('CW06ADWORDS_GA_CONVERSION_ID',Tools::getValue('GA_CONVERSION_ID'));
+            Configuration::updateValue('CW06ADWORDS_GA_CONVERSION_LABEL',Tools::getValue('GA_CONVERSION_LABEL'));
+            $this->_html .= $this->displayConfirmation('Paramètres enregistrés avec succès');
+        }
+        if(Tools::isSubmit('submitConfigFB')){
+			Configuration::updateValue('CW06ADWORDS_FB_CONVERSION_ID',Tools::getValue('FB_CONVERSION_ID'));
             $this->_html .= $this->displayConfirmation('Paramètres enregistrés avec succès');
         }
         
         $this->_html .= '<fieldset>'
-                . '<legend>'.$this->l('Configuration').'</legend>'
+                . '<legend>'.$this->l('Google Analytics').'</legend>'
                 . '<form method="POST">'
                 . '<label>'.$this->l('Conversion ID').'</label>'
                 . '<div class="margin-form">'
-                . '<input type="text" name="CONVERSION_ID" value="'.Configuration::get('CW06ADWORDS_CONVERSION_ID').'" />'
+                . '<input type="text" name="GA_CONVERSION_ID" value="'.Configuration::get('CW06ADWORDS_GA_CONVERSION_ID').'" />'
                 . '</div>'
                 . '<label>'.$this->l('Conversion Label').'</label>'
                 . '<div class="margin-form">'
-                . '<input type="text" name="CONVERSION_LABEL" value="'.Configuration::get('CW06ADWORDS_CONVERSION_LABEL').'" />'
+                . '<input type="text" name="GA_CONVERSION_LABEL" value="'.Configuration::get('CW06ADWORDS_GA_CONVERSION_LABEL').'" />'
                 . '</div>'
                 . '<div class="margin-form">'
-                . '<input type="submit" name="submitConfig" value="'.$this->l('Enregistrer').'" />'
+                . '<input type="submit" name="submitConfigGA" value="'.$this->l('Enregistrer').'" />'
                 . '</div>'
                 . '</form>'
                 . '</fieldset>';
-        
+
+        $this->_html .= '<fieldset>'
+                . '<legend>'.$this->l('Facebook').'</legend>'
+                . '<form method="POST">'
+                . '<label>'.$this->l('Conversion ID').'</label>'
+                . '<div class="margin-form">'
+                . '<input type="text" name="FB_CONVERSION_ID" value="'.Configuration::get('CW06ADWORDS_FB_CONVERSION_ID').'" />'
+                . '</div>'
+                . '<div class="margin-form">'
+                . '<input type="submit" name="submitConfigFB" value="'.$this->l('Enregistrer').'" />'
+                . '</div>'
+                . '</form>'
+                . '</fieldset>';
+		
         return $this->_html;
     }
     
-    
-    
-    
-    
     public function hookOrderConfirmation($params){       
         global $smarty;
-        $smarty->assign('CONVERSION_ID',Configuration::get('CW06ADWORDS_CONVERSION_ID'));
-        $smarty->assign('CONVERSION_LABEL',Configuration::get('CW06ADWORDS_CONVERSION_LABEL'));
+        $smarty->assign('GA_CONVERSION_ID',Configuration::get('CW06ADWORDS_GA_CONVERSION_ID'));
+        $smarty->assign('GA_CONVERSION_LABEL',Configuration::get('CW06ADWORDS_GA_CONVERSION_LABEL'));
+        $smarty->assign('FB_CONVERSION_ID',Configuration::get('CW06ADWORDS_FB_CONVERSION_ID'));
         $smarty->assign('CONVERSION_AMOUNT',$params['objOrder']->total_paid);
         return $this->display(__FILE__, 'orderConfirmation.tpl');
     }
--- orderConfirmation.tpl
+++ orderConfirmation.tpl
@@ -1,22 +1,66 @@
+{if $GA_CONVERSION_ID && $GA_CONVERSION_LABEL }
+	<!-- Google Code for Vente Conversion Page -->
+	<script type="text/javascript">
+	/* <![CDATA[ */
+	var google_conversion_id = "{$GA_CONVERSION_ID}";
+	var google_conversion_language = "{$lang_iso}";
+	var google_conversion_format = "3";
+	var google_conversion_color = "ffffff";
+	var google_conversion_label = "{$GA_CONVERSION_LABEL}";
+	var google_conversion_amount = {$CONVERSION_AMOUNT};
 
-<!-- Google Code for Vente Conversion Page -->
-<script type="text/javascript">
-/* <![CDATA[ */
-var google_conversion_id = {$CONVERSION_ID};
-var google_conversion_language = "{$lang_iso}";
-var google_conversion_format = "3";
-var google_conversion_color = "ffffff";
-var google_conversion_label = "{$CONVERSION_LABEL}";
-if ({$CONVERSION_AMOUNT}) {
-  var google_conversion_value = {$CONVERSION_AMOUNT};
-}
-var google_remarketing_only = false;
-/* ]]> */
-</script>
-<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js">
-</script>
-<noscript>
-<div style="display:inline;">
-<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/{$CONVERSION_ID}/?value={$CONVERSION_AMOUNT}&amp;label={$CONVERSION_LABEL}&amp;guid=ON&amp;script=0"/>
-</div>
-</noscript>
+	{literal}
+	if (google_conversion_amount) {
+	  var google_conversion_value = google_conversion_amount;
+	}
+	var google_remarketing_only = false;
+	/* ]]> */
+	{/literal}
+	</script>
+	<script type="text/javascript" src="//www.googleadservices.com/pagead/conversion.js"></script>
+	<noscript>
+	<div style="display:inline;">
+	<img height="1" width="1" style="border-style:none;" alt="" src="//www.googleadservices.com/pagead/conversion/{$GA_CONVERSION_ID}/?value={$CONVERSION_AMOUNT}&amp;label={$GA_CONVERSION_LABEL}&amp;guid=ON&amp;script=0"/>
+	</div>
+	</noscript>
+
+	<script>
+	{literal}
+	(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	ga('create', 'UA-54925665-1', 'auto');
+	ga('send', 'pageview');
+	{/literal}
+	</script>
+{/if}
+
+{if $FB_CONVERSION_ID }
+	<!-- Facebook Conversion Code for Facebook -->
+	<script>
+	fbPixelTracking = {$FB_CONVERSION_ID};
+	conversionAmount = {$CONVERSION_AMOUNT};
+	currency = {$currency->iso_code};
+	{literal}
+	(function() {
+		var _fbq = window._fbq || (window._fbq = []);
+		if (!_fbq.loaded) {
+			var fbds = document.createElement('script');
+			fbds.async = true;
+			fbds.src = '//connect.facebook.net/en_US/fbds.js';
+			var s = document.getElementsByTagName('script')[0];
+			s.parentNode.insertBefore(fbds, s);
+			_fbq.loaded = true;
+		}
+	})();
+	window._fbq = window._fbq || [];
+	window._fbq.push(['track', fbPixelTracking, {
+		'value':conversionAmount,
+		'currency':currency
+	}]);
+	{/literal}
+	</script>
+	<noscript><img height="1" width="1" alt="" style="display:none" src="https://www.facebook.com/tr?ev={$FB_CONVERSION_ID}&amp;cd[value]={$CONVERSION_AMOUNT}&amp;cd[currency]={$currency->iso_code}&amp;noscript=1" /></noscript>
+{/if}
